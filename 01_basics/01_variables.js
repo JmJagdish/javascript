f2c5a8825dc473087f95cf53b31aa26f6ea0c0a8
Jagdish Mahanta <43695894+JmJagdish@users.noreply.github.com>
2023-12-07 00:39:27 +0530

    datatypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -15,13 +15,3 @@ accountCity = "Bangalore";
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
 
 
-// number => 2 to 53
-// bigint
-// string => ""
-// boolean => true/false
-// null => standalone value and it's type is object
-// undefiend => value not assigned it's type is undefiend
-// symbol => unique character
-
-
-// object 
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,10 @@
+// number => 2 to 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value and it's type is object
+// undefiend => value not assigned it's type is undefiend
+// symbol => unique character
+
+
+// object 
