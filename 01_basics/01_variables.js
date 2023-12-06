213a8bfa87351c1ec1ff2832e090ba07342b4670
Jagdish Mahanta <43695894+JmJagdish@users.noreply.github.com>
2023-12-07 00:36:47 +0530

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,27 @@
+const accountId = 12345;
+let accountEmail = "jagdish@123gmail.com";
+var accountPassword = "123456";
+
+
+accountCity = "Jaipur";
+// accountId = "6789";  can't change the value of variable declare by const(constant) keyword.
+
+let accountState;
+// If you declare a variable but not assigning any value to it then It will show undefiend.
+
+accountEmail = "sudhansu@gmail.com";
+accountPassword = "98765";
+accountCity = "Bangalore";
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+
+// number => 2 to 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value and it's type is object
+// undefiend => value not assigned it's type is undefiend
+// symbol => unique character
+
+
+// object 
---
+++ 01_basics/test.js
@@ -0,0 +1,1 @@
+console.log("Jagdish and sudhansu");
