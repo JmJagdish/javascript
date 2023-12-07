9c96b9ef6e1107e4b13c436cb9a61f182fe0d363
Jagdish Mahanta <43695894+JmJagdish@users.noreply.github.com>
2023-12-07 20:01:27 +0530

    comparision oprator

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -12,7 +12,7 @@ let valueInNumber = Number(score);
 
 let isLoggedIn = 0;
 let booleanLoggedIn = Boolean(isLoggedIn);
-console.log(booleanLoggedIn);
+// console.log(booleanLoggedIn);
 
 // 1 => true; 0 > false
 // "" => false
@@ -21,6 +21,52 @@ console.log(booleanLoggedIn);
 let someNumber = 33;
 
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+/* *******************Operation************************ */
+let value = 5;
+let negValue = -value;
+
+// console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 3);
+// console.log(2 / 2);
+// console.log(2 % 2);
+
+
+let str1 = "Hello";
+let str2 = "Jagdish";
+let str3 = str1 + " " + str2;
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// Because it will add    Number first then string
+
+// console.log((3 + 4) * 5 % 3);
+
+// using bracket is best way to write a code in project
+// because readable code is required for companies
+
+console.log(+true);
+console.log(+false);
+console.log(+"");
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+// gameCounter++;
+// console.log(gameCounter);
+++gameCounter;
+console.log(gameCounter);
 
