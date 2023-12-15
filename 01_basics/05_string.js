22aab4ee3f66d682f929e3f97c0c78167f0f82fb
Jagdish Mahanta <43695894+JmJagdish@users.noreply.github.com>
2023-12-15 12:59:47 +0530

    arrays slice and splice

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -9,36 +9,36 @@ const repoCount = 50;
 
 // Delare String new way
 
-const gameName = new String('I+Love+to+play+Basketball');
+const gameName = new String('I Love to play+Basketball');
 
 // console.log(gameName);
 // console.log(`${gameName.toUpperCase()}`);
 
-console.log(gameName[0]);
-console.log(gameName.__proto__);
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
 
-console.log(gameName.length);
-console.log(gameName.toUpperCase());
-console.log(gameName.charAt(3));
-console.log(gameName.indexOf('k'));
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('k'));
 
 const newString = gameName.substring(0, 9);
-console.log(newString);
+// console.log(newString);
 
-const anotherString = gameName.slice(-10, 10);
-console.log(anotherString);
+// const anotherString = gameName.slice(-10, 10);
+// console.log(anotherString);
 
-const newStringOne = "   Jagdish   ";
-console.log(newStringOne);
-console.log(newStringOne.trim());
+// const newStringOne = "   Jagdish   ";
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
 
-const url = "https;//jagdish.com/jagdish%20mahanta";
+// const url = "https;//jagdish.com/jagdish%20mahanta";
 
-console.log(url.replace('%20', '-'));
+// console.log(url.replace('%20', '-'));
 
-console.log(url.includes('jagdish'));
-console.log(url.includes('niket'));
+// console.log(url.includes('jagdish'));
+// console.log(url.includes('niket'));
 
 
-console.log(gameName.split('+'));
+console.log(gameName.split(' '));
 
---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,38 @@
+// arrays
+
+// const myArr = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
+// const myHeros = ['batman', 'ironman'];
+
+// const myArr2 = new Array(1, 2, 3, 4);
+// console.log(myArr2);
+
+// Array methods
+
+// myArr.push(10);
+// myArr.pop();
+
+
+// console.table(myArr.includes(5));
+// console.table(myArr.indexOf(6));
+
+// const newArr = myArr.join();
+// console.log(newArr);
+// console.log(typeof newArr);
+
+
+// slice , splice
+
+const myArr = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
+console.log('A', myArr);
+
+const myn1 = myArr.slice(1, 3);
+console.log(myn1);
+console.log('B', myArr);
+
+const myn2 = myArr.splice(1, 3);
+console.log('C', myArr);
+console.log(myn2);
+
+
